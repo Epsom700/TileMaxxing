ded81a7dc76985fa15e1b474dc33ae80cf1a1d26
Epsom700 <arjun.jt11d@gmail.com>
2026-05-24 03:29:50 +0530

    added create DecisionNode, ChanceNode and select functions to MCTS

--- src/mcts.cpp
+++ src/mcts.cpp
@@ -3,13 +3,14 @@
 #include "board.h"
 #include <limits>
 #include <cmath>
+#include <random>
 using namespace std;
 
-MCTS::MCTS(int simulation) : simulation_(simulation) {
+MCTS::MCTS(int simulation) : simulation_(simulation), gen_(random_device{}()) {
 
 }
 
-double MCTS::ucb1(decisionNode* root){
+double MCTS::ucb1(chanceNode* root){
     // UCB1 = (total_score / visit_count) + C * sqrt(ln(parent_visits) / visit_count)
     if (root->visit_counter==0)return numeric_limits<double>::infinity(); 
     if (root->parent==nullptr) return numeric_limits<double>::infinity(); 
@@ -22,7 +23,81 @@ double MCTS::ucb1(decisionNode* root){
     return value; 
 
 }
+pair<MCTS::decisionNode*, MCTS::chanceNode*> MCTS::select(decisionNode* root){
+    decisionNode* track = root; 
+    while ((!track->child.empty())&&(track->is_terminal==false)){
+        double best_value = -numeric_limits<double>::infinity();
+        chanceNode* best_node = nullptr; 
+        for (auto* c : track->child){
+            double temp = ucb1(c); 
+            if (best_value < temp){
+                best_value = temp; 
+                best_node = c; 
+            }
+        }
+
+        vector<pair<int, int>> zero_track; 
+        auto& b = best_node->board;
+        for (int i = 0; i< 4; ++i){
+            for (int j=0; j<4;++j){
+                if (b[i][j]==0) zero_track.push_back({i, j}); 
+            }
+        }
+ 
+        int len_track_loop = zero_track.size(); 
+        discrete_distribution<int> distr_2({90, 10}); 
+        uniform_int_distribution<int> distr(0, len_track_loop-1);
+        int index = distr(gen_);
+        pair<int, int> cell =zero_track[index]; 
+        int tile = (distr_2(gen_) == 0) ? 2 : 4; 
+        pair<int, pair<int, int>> tile_val_pos = {tile, cell};
+        auto it = best_node->children.find(tile_val_pos); 
+        if (it != best_node->children.end()){
+            track = it->second; 
+        }else{
+            last_sampled_key_ = tile_val_pos;
+            return {track, best_node}; 
+        }
+    }
+    return {track, nullptr};
+
+}
+
+
+MCTS::decisionNode* MCTS::createDecisionNode(chanceNode* parent, pair<int, pair<int, int>> tile_val_pos){
+    decisionNode* new_node = new decisionNode; 
+    new_node->parent = parent; 
+    memcpy(new_node->board, parent->board, sizeof(parent->board));
+    pair<int, int> tile_pos_ = tile_val_pos.second; 
+    new_node->board[tile_pos_.first][tile_pos_.second] = tile_val_pos.first; 
+    new_node->tile_pos = tile_pos_; 
+    new_node->tile_val = tile_val_pos.first; 
+    new_node->visit_counter = 0; 
+    new_node->total_score = 0; 
+    Board temp;
+    memcpy(temp.board, new_node->board, sizeof(temp.board));
+    new_node->is_terminal = temp.isGameOver();
+    parent->children[tile_val_pos] = new_node; 
+    return new_node; 
+}
+
+MCTS::chanceNode* MCTS::createChanceNode(decisionNode* parent, int move, const int board_after_move[4][4]){
+    chanceNode* new_node = new chanceNode; 
+    memcpy(new_node->board, board_after_move, sizeof(new_node->board)); 
+    new_node->move = move; 
+    new_node->visit_counter = 0; 
+    new_node->total_score = 0; 
+    new_node->parent = parent; 
+    parent->child.push_back(new_node); 
+    return new_node; 
+
+}
+
+MCTS::decisionNode* MCTS::expand(pair<decisionNode*, chanceNode*> frontier){
+
+}
+
 
 int MCTS::getBestMove(Board& board){
-
+    
 }
--- src/mcts.h
+++ src/mcts.h
@@ -1,32 +1,42 @@
 #include <vector>
+#include <map>
+#include <utility>
 #include "board.h"
 class MCTS{
     public: 
-        MCTS(int simulation_); 
+        MCTS(int simulation_);
+        enum Directions {left = 0, right, up, down}; 
         int getBestMove(Board& board); 
         struct chanceNode; 
         struct decisionNode
         {
             int board[4][4]; 
-            int move;
             int visit_counter; 
+            int tile_val; 
+            std::pair<int, int> tile_pos; 
             double total_score; 
-            decisionNode* parent; 
+            chanceNode* parent; 
+            bool is_terminal;
             std::vector<chanceNode*> child; 
         };
         struct chanceNode
         {
             int board[4][4]; 
-            int tile_val; 
-            std::pair<int,int> tile_position; 
+            int move; 
             decisionNode* parent; 
-            decisionNode* child; 
+            std::map<std::pair<int,std::pair<int,int>>, decisionNode*> children;
             int visit_counter; 
             double total_score; 
         };
 
     private: 
         int simulation_; 
-        void deleteTrees(decisionNode* root);         
-        double ucb1(decisionNode* root); 
+        void deleteTrees(decisionNode* root);  
+        std::pair<decisionNode*, chanceNode*> select(decisionNode* root);  
+        decisionNode* expand(std::pair<decisionNode*, chanceNode*> node); 
+        decisionNode* createDecisionNode(chanceNode* parent, std::pair<int, std::pair<int, int>> tile_val_pos);
+        chanceNode* createChanceNode(decisionNode* parent, int move, const int board_after_move[4][4]);  
+        double ucb1(chanceNode* root); 
+        std::mt19937 gen_;
+        std::pair<int, std::pair<int, int>> last_sampled_key_; 
 };
